808e9661e7cd443908c5feb812aff268fc1dfdf7
saurabh singh <saurabhsingh100605@gmail.com>
2026-05-26 12:25:08 +0530

    refactor: optimise metadata and config references

--- app/components/link/link.stories.jsx
+++ app/components/link/link.stories.jsx
@@ -7,8 +7,8 @@ export default {
 
 export const Default = () => (
   <StoryContainer style={{ fontSize: 18 }}>
-    <Link href="https://hamishw.com">Primary link</Link>
-    <Link secondary href="https://hamishw.com">
+    <Link href="https://devsphere.dev">Primary link</Link>
+    <Link secondary href="https://devsphere.dev">
       Secondary link
     </Link>
   </StoryContainer>
